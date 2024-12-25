29d718bf474cfddde635da879d8ed9f52b18f6c8
kkx7787 <kkx7787@naver.com>
2024-12-26 04:07:58 +0900

    fix: 서울시 전체 데이터 조회

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ def get_value(data, keys, default=None):
 # MySQL에 데이터 삽입
 connection = connect_to_mysql()
 if connection:
-    city = ['금천구']
+    city = ['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구',
+            '성동구',
+            '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구']
 
     for gu in city:
         gu_location_data = fetch_location_data(gu, header)
